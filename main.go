b75f9e8dca21a47c1ccf7c820a187ff57cc99f87
Gabo <gabo.alucema@gmail.com>
2025-06-07 23:24:33 -0400

    configuracion inicial

--- main.go
+++ main.go
@@ -2,11 +2,23 @@ package main
 
 import "fmt"
 import "github.com/gin-gonic/gin"
+import "github.com/joho/godotenv"
+import "os"
+import "log"
+import "db/db.go"
 
 func main() {
-	fmt.Println("Corriendo en localhost:8080 !!!")
-	
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error al cargar .env")
+	}
+
+	db.Conectar()
+	fmt.Println("Conexión a la base de datos exitosa")
+
   	router := gin.Default()
+//	router.Static("/static", "./static")
+
   	router.GET("/", func(c *gin.Context) {
   	  	c.JSON(200, gin.H{
   	    	"message": "pong",
