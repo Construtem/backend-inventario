2bbaae73bd522f022aa27f3b0fb21d8fb3d6c68a
Gabo <gabo.alucema@gmail.com>
2025-06-07 23:23:19 -0400

    creada inicializacion bd

---
+++ db/db.go
@@ -0,0 +1,30 @@
+package db
+
+import (
+	"fmt"
+	"os"
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Conectar() {
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable TimeZone=America/Santiago", host, port, user, password, dbname)
+
+	conn, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Error al conectar a la base de datos:", err)
+	}
+
+	log.Println("Conexión exitosa a la base de datos")
+	DB = conn
+}
