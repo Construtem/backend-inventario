fe4cc9f0bb667cd2785a7f12ca03b8338be67c67
Gabo <gabo.alucema@gmail.com>
2025-06-10 00:52:35 -0400

    configurado gorm logger

--- api/db/db.go
+++ api/db/db.go
@@ -7,6 +7,7 @@ import (
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 var DB *gorm.DB
@@ -18,12 +19,26 @@ func Conectar() {
 	password := os.Getenv("DB_PASSWORD")
 	dbname := os.Getenv("DB_NAME")
 
-	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable TimeZone=America/Santiago", host, port, user, password, dbname)
-
-	conn, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatal("Error al conectar a la base de datos:", err)
+	if host == "" || port == "" || user == "" || password == "" || dbname == "" {
+		log.Fatal("Faltan variables de entorno para la conexión a la base de datos")
 	}
 
-	DB = conn
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable TimeZone=America/Santiago", host, port, user, password, dbname)
+
+	gormLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		logger.Config{
+			LogLevel: logger.Silent,
+			Colorful: true,
+		},
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: gormLogger,
+	})
+	if err != nil {
+		log.Fatalf("Error al conectar a la base de datos: %v", err)
+	}
+
+	DB = db
 }
